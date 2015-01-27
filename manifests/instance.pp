78e32547dbb68db77d2e1c9e45ccb680a3e376d7
Matthew Scharley <matthew.scharley@equiem.com.au>
2015-01-28 10:22:51 +1100

    Qualify test as it's not recognised as a shell builtin

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -4,16 +4,18 @@ define drupal_solr::instance(
 ) {
   case $ensure {
     present: {
-      exec { "/bin/bash /usr/local/bin/create-solr-instance ${name} 7 sapi":
+      exec { "create-solr-instance ${name} 7 sapi":
         require => File['/usr/local/bin/create-solr-instance'],
         user => 'root',
+        path => ['/bin', '/usr/bin', '/usr/local/bin'],
         unless => "test -d /opt/solr/solr-${name}",
       }
     }
     absent: {
-      exec { "/bin/bash /usr/local/bin/remove-solr-instance ${name}":
+      exec { "remove-solr-instance ${name}":
         require => File['/usr/local/bin/remove-solr-instance'],
         user => 'root',
+        path => ['/bin', '/usr/bin', '/usr/local/bin'],
         onlyif => "test -d /opt/solr/solr-${name}",
       }
     }
